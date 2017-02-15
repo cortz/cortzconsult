2bbad92dcbfef7b2f0a3c8c8688adecb1f89de3c
Mads Cortz Andreasen <mca@worldmanuals.com>
2017-02-15 14:10:12 +0100

    added smooth scroll

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -8,11 +8,11 @@ class Contact extends Component {
 
     render() {
         return (
-            <div className="contact">
+            <div>
                 <h3>Want to work together?</h3>
                 <p>I'm currently accepting new projects and would love to hear about yours.<br /> 
                 Please take a few minutes to tell me about it.</p>
-                <a href="" className="my-btn">Contact me!</a>
+                <a href="mailto:mads@cortzconsult.dk" className="my-btn">Contact me!</a>
             </div>
         )
     }
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -1,20 +1,31 @@
 import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import {IndexLink, browserHistory} from 'react-router';
 import {Grid, Row, Col, MenuItem} from 'react-bootstrap';
+import Scroll from 'react-scroll';
+
+import me from '../../images/me.jpg';
 
 class Layout extends Component {
     constructor(props) {
         super(props);
+
+        this.scrollToBottom = this.scrollToBottom.bind(this);
+    }
+
+    scrollToBottom() {
+        Scroll.animateScroll.scrollToBottom();
     }
 
     render() {
         return (
             <div className="app">
-                <Grid fluid="true">
+                <Grid fluid={true}>
                     <Row>
                         <Col lg={12} md={12} sm={12} xs={12} className="header">
+                            <img src={me} />
                             <h1>Hi, I'm Mads. I build web applications.</h1>
-                            <a href="mailto:mads@cortzconsult.dk" className="my-btn">Yes I'm available for hire!</a>
+                            <a href="#" className="my-btn" onClick={this.scrollToBottom}>Yes I'm available for hire!</a>
                         </Col>
                         
                         {this.props.children}
