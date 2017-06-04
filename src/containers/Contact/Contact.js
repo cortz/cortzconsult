cd36822a5afb9fd7bb95a9adb453f013f70d1b18
Mads Cortz Andreasen <mca@worldmanuals.com>
2017-06-04 14:16:56 +0200

    updated projects

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -13,6 +13,10 @@ class Contact extends Component {
                 <p>I'm currently accepting new projects and would love to hear about yours.<br /> 
                 Please take a few minutes to tell me about it.</p>
                 <a href="mailto:mads@cortzconsult.dk" className="my-btn">Contact me!</a>
+                <div className="skills">
+                    <h4>I'm really good at</h4>
+                    <code>C#, ASP.NET, jQuery, SQL, HTML5, CSS3, SCSS, LESS, AWS, CDN, JavaScript, Git</code>
+                </div>
             </div>
         )
     }
--- src/containers/Projects/components/Project.js
+++ src/containers/Projects/components/Project.js
@@ -2,37 +2,24 @@ import React, {Component} from 'react';
 import {IndexLink, browserHistory} from 'react-router';
 import {Grid, Row, Col, MenuItem} from 'react-bootstrap';
 
+import droppointImage from '../../../images/droppoint.png';
+
 export default function Project() {
     return (
         <div>
             <Row className="project">
+                
                 <Col md={6} sm={12} xs={12}>
-                    <h3>Lorem Ipsum</h3>
+                    <h3>Choose drop off point</h3>
                     <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et sagittis felis. Quisque sed iaculis tellus. Duis sit amet dolor ut tortor porttitor accumsan. Pellentesque venenatis, risus non convallis rutrum, felis felis pharetra lacus, at imperdiet arcu augue quis tellus. Nam tincidunt bibendum venenatis. Suspendisse eu convallis nisi, vel consectetur sem. Ut sagittis dignissim auctor. Donec pulvinar gravida accumsan. Maecenas porttitor in arcu non ultricies.
-
-                        rhoncus. Vestibulum porttitor auctor orci, sed ornare dolor ultrices non. Nulla non massa fermentum quam tempor tincidunt at eget purus. Pellentesque at augue a dolor mattis varius id ut justo. Maecenas eu urna id nisl interdum tristique et quis nunc. Ut eu consectetur nunc. Nam at posuere orci.
-                    </p>
-                    <p>
-                        <cite><span>“</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit.<span>”</span></cite>
-                    </p>
-                </Col>
-                <Col md={6} sm={12} xs={12}>
-                    <img src="https://digitalsynopsis.com/wp-content/uploads/2014/03/how-to-create-a-website-feature-image.jpg" />
-                </Col>
-            </Row>
-            <Row className="project">
-                <Col md={6} sm={12} xs={12}>
-                    <h3>Lorem Ipsum</h3>
-                    <p>
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc et sagittis felis. Quisque sed iaculis tellus. Duis sit amet dolor ut tortor porttitor accumsan. Pellentesque venenatis, risus non convallis rutrum, felis felis pharetra lacus, at imperdiet arcu augue quis tellus. Nam tincidunt bibendum venenatis. Suspendisse eu convallis nisi, vel consectetur sem. Ut sagittis dignissim auctor. Donec pulvinar gravida accumsan. Maecenas porttitor in arcu non ultricies.
-                    </p>
-                    <p>
-                        <cite><span>“</span> Lorem ipsum dolor sit amet, consectetur adipiscing elit.<span>”</span></cite>
+                        Small project where you are able to choose where you want your package to be dropped off. <br/>
+                        It's build with <strong>bootstrap</strong> and <strong>jQuery</strong>.<br/>
+                        Data is pulled from bring and Shippii then shown on a map from <strong>Google Maps API</strong>.<br/><br/>
+                        Time used: <strong>8 hours.</strong>
                     </p>
                 </Col>
                 <Col md={6} sm={12} xs={12}>
-                    <img src="https://digitalsynopsis.com/wp-content/uploads/2014/03/how-to-create-a-website-feature-image.jpg" />
+                    <img src={droppointImage} />
                 </Col>
             </Row>
         </div>
